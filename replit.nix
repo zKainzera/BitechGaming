5ee194dd55668d22b77a9801f3696639f10f8ec9
Arthur <artmartins28@gmail.com>
2023-10-25 12:36:26 -0300

    git push

--- replit.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs }: {
-  deps = [
-    pkgs.sass
-    pkgs.[15~[15~
-    pkgs.nodejs-16_x
-    pkgs.nodePackages.vscode-langservers-extracted
-    pkgs.nodePackages.typescript-language-server  
-  ];
-}

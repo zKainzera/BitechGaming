29375758924b896f16b88dc7559ae1b300b27665
AdrianFSilva <adrian.adriansilva06@gmail.com>
2023-11-26 21:14:36 -0300

    muitas coisas
    mudança no cadastro e pequenos detalhes adicionados a outras páginas como footer

--- JavaScript/cadastro.js
+++ JavaScript/cadastro.js
@@ -1,12 +1,16 @@
 let $id = id => document.getElementById(id);
-var [login, register, form] = ['login', 'register', 'form'].map(id => $id(id));
+let [login, register, form] = ['login', 'register', 'form'].map(id => $id(id));
 
-[login, register].map(element => {
+[login, register].forEach(element => {
   element.onclick = function () {
-    [login, register].map($ele => {
+    [login, register].forEach($ele => {
       $ele.classList.remove("active");
     });
     this.classList.add("active");
-    this.getAttribute("id") === "register" ? form.classList.add("active") : form.classList.remove("active");
+    if (this.getAttribute("id") === "register") {
+      form.classList.add("active");
+    } else {
+      form.classList.remove("active");
+    }
   };
 });
